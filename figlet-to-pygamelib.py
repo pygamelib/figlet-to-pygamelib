3f705ff4c05a2dc89134af06a8ddb644e5c42a72
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2022-05-22 16:52:52 -0700

    Add support for argparse to correctly handle CLI options.

--- figlet-to-pygamelib.py
+++ figlet-to-pygamelib.py
@@ -1,8 +1,9 @@
 from pyfiglet import Figlet
 from pygamelib.gfx import core
-import sys
+from pygamelib import base, engine
 import os
 import json
+import argparse
 
 readme_tmpl = """
 # figlet-$font_name
@@ -23,8 +24,76 @@ Each font is the property of its respective author. This is just an adaptation t
 pygamelib font format.
 """
 
+
+parser = argparse.ArgumentParser(
+    description="A tool to convert a Figlet font into a pygamelib sprite-based font. "
+    "Please read https://github.com/arnauddupuis/pygamelib/wiki/Font-creation for more "
+    "information."
+)
+
+parser.add_argument(
+    "--scalable",
+    action=argparse.BooleanOptionalAction,
+    default=False,
+    help="Declare the font as scalable. You have to make sure that it is actually "
+    "scalable.",
+)
+
+parser.add_argument(
+    "--monospace",
+    action=argparse.BooleanOptionalAction,
+    default=False,
+    help="Declare the font as monospace. It means that all characters have the same "
+    "width. In that case, the width of the font is the width of the widest character.",
+)
+
+# parser.add_argument(
+#     "--colorable",
+#     action=argparse.BooleanOptionalAction,
+#     default=True,
+#     help="Declare the font as colorable. This means that the font is created using 2 "
+#     "colors (background and foreground) and that the Font class will be able to use"
+#     " this to allow colorization of the font. Read: "
+#     "https://github.com/arnauddupuis/pygamelib/wiki/Font-creation for more info.",
+# )
+
+parser.add_argument(
+    "--output-directory",
+    type=str,
+    action="store",
+    required=False,
+    default="",
+    help="Specify the output directory. If set, all files generated will be store in "
+    "that directory. If not, files are going to be stored in the current directory. "
+    "The entire directory structure will be created if it does not exist.",
+)
+
+parser.add_argument(
+    "--space-width",
+    type=int,
+    action="store",
+    required=False,
+    default=0,
+    help="Specify the space character width. Sometimes the one in the Figlet font does "
+    "not work/look good for the game. You can define the width of the space character "
+    "and test different values.",
+)
+
+parser.add_argument(
+    "--test",
+    action=argparse.BooleanOptionalAction,
+    default=False,
+    help="Show a test rendering of the freshly created font.",
+)
+
+parser.add_argument(
+    "font_name", help="The name of the Figlet font to convert. Ex: univers"
+)
+
+args = parser.parse_args()
+
 config = {
-    "scalable": False,
+    "scalable": args.scalable,
     "monospace": False,
     "colorable": True,
     "height": 11,
@@ -35,9 +104,11 @@ config = {
     "glyphs_map": {},
 }
 
-font_name = "univers"
-if len(sys.argv) > 1:
-    font_name = sys.argv[1]
+font_name = args.font_name
+
+output_dir = "."
+if args.output_directory:
+    output_dir = args.output_directory
 
 print(f"Loading font: {font_name}...", end="")
 fl = Figlet(font=font_name)
@@ -45,6 +116,17 @@ print("done")
 config["height"] = fl.Font.height
 readme_tmpl = readme_tmpl.replace("$font_name", font_name)
 readme_tmpl = readme_tmpl.replace("$figfontcomment", fl.Font.comment)
+
+maxwidth = 0
+for wi in fl.Font.width:
+    if fl.Font.width[wi] > maxwidth:
+        maxwidth = fl.Font.width[wi]
+space_desired_width = maxwidth
+
+if args.monospace:
+    config["monospace"] = True
+    config["width"] = maxwidth
+
 
 fc = core.SpriteCollection()
 
@@ -54,17 +136,20 @@ print("Generating sprites...", end="")
 for char in fl.Font.chars:
     spr = None
     if chr(char) == " ":
-        if fl.Font.width[char] <= fl.Font.width[ord("a")]:
+        if fl.Font.width[char] <= space_desired_width:
             # This just doesn't work for our font system, so we create a bigger
             # space.
             spr = core.Sprite(
-                size=[fl.Font.width[ord("a")], fl.Font.height],
+                size=[space_desired_width, fl.Font.height],
                 default_sprixel=core.Sprixel(" "),
                 name=" ",
             )
     if spr is None:
+        char_width = fl.Font.width[char]
+        if args.monospace:
+            char_width = config["width"]
         spr = core.Sprite(
-            size=[fl.Font.width[char], fl.Font.height],
+            size=[char_width, fl.Font.height],
             default_sprixel=core.Sprixel(" "),
         )
         row = 0
@@ -81,12 +166,25 @@ for char in fl.Font.chars:
     fc.add(spr)
 print("done")
 print("Writting files...", end="")
-os.makedirs(f"pygamelib/assets/fonts/figlet-{font_name}", exist_ok=True)
-fc.to_json_file(f"pygamelib/assets/fonts/figlet-{font_name}/glyphs.spr")
-with open(f"pygamelib/assets/fonts/figlet-{font_name}/config.json", "w") as file:
+os.makedirs(f"{output_dir}/pygamelib/assets/fonts/figlet-{font_name}", exist_ok=True)
+fc.to_json_file(f"{output_dir}/pygamelib/assets/fonts/figlet-{font_name}/glyphs.spr")
+with open(
+    f"{output_dir}/pygamelib/assets/fonts/figlet-{font_name}/config.json", "w"
+) as file:
     json.dump(config, file)
 
-with open(f"pygamelib/assets/fonts/figlet-{font_name}/Readme.md", "w") as file:
+with open(
+    f"{output_dir}/pygamelib/assets/fonts/figlet-{font_name}/Readme.md", "w"
+) as file:
     file.write(readme_tmpl)
 
 print("done")
+
+print("Here is a test of the font:")
+s = engine.Screen(height=config["height"] * 3)
+t = base.Text(
+    f"Test of the:\nfiglet-{font_name}\nfont",
+    font=core.Font(f"figlet-{font_name}", [f"{output_dir}/pygamelib/assets/"]),
+)
+s.place(t, 0, 0)
+s.update()
